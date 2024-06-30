6691853d633bc8c9e16faf618f7a4f872ba6cf74
Ansh Bhasin <74207421+ItzAnsh@users.noreply.github.com>
2024-06-30 13:48:31 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -139,7 +139,7 @@ export default function Home() {
 				<Section3 />
 				<Section4 />
 				<Section5 />
-				<Section6 />
+				// <Section6 />
 			</main>
 		</>
 		// {/* </LocomotiveScrollProvider> */}
